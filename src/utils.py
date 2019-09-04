c693aa79fc25b1dd582d378ebe96fd62b658b2c8
Ines Diaz Bessone <ine.db84@gmail.com>
2019-09-04 19:46:50 -0300

    agrego un show_df

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,10 @@ import pandas as pd
 def hello_world():
     print ('')
     print ('hello world'!)
+    
+
+def show_df(df):
+    print(df.shape)
+    display(df.head())
+    display(df.describe())
                 
